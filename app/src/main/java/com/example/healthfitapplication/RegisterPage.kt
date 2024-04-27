b893f1c2018addd477c2338b7b6fb1f75d4187cd
farhaankaleem <farhaan.kaleem@gmail.com>
2024-04-27 06:06:10 +0100

    registration with user information

--- app/src/main/java/com/example/healthfitapplication/RegisterPage.kt
+++ app/src/main/java/com/example/healthfitapplication/RegisterPage.kt
@@ -4,18 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.example.healthfitapplication.databinding.ActivityRegisterPageBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 
 class RegisterPage : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterPageBinding
-    val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterPageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.signIn.setOnClickListener {
             val intent = Intent(this, LoginPage::class.java)
@@ -26,6 +31,18 @@ class RegisterPage : AppCompatActivity() {
         binding.signUp.setOnClickListener {
             var email: String = binding.email.text.toString()
             var password: String = binding.password.text.toString()
+            var fName: String = binding.firstName.text.toString()
+            var lName: String = binding.lastName.text.toString()
+
+            if (TextUtils.isEmpty(fName)) {
+                Toast.makeText(this, "Enter First Name", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (TextUtils.isEmpty(lName)) {
+                Toast.makeText(this, "Enter Last Name", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
 
             if (TextUtils.isEmpty(email)) {
                 Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show()
@@ -37,12 +54,31 @@ class RegisterPage : AppCompatActivity() {
                 return@setOnClickListener
             }
 
+            val name = "$fName $lName"
+
+            binding.progressBar.visibility = View.VISIBLE
+
             firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                binding.progressBar.visibility = View.INVISIBLE
                 if (it.isSuccessful) {
-                    Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, LoginPage::class.java)
-                    startActivity(intent)
-                    finish()
+                    val user = firebaseAuth.currentUser
+                    val profileUpdates = UserProfileChangeRequest.Builder()
+                        .setDisplayName(name)
+                        .build()
+
+                    user?.updateProfile(profileUpdates)
+                        ?.addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                Log.d("Registration", "User profile updated.")
+                                Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this, LoginPage::class.java)
+                                startActivity(intent)
+                                finish()
+                            } else {
+                                Log.e("Registration", "Failed to update user profile.")
+                                Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
+                            }
+                        }
                 } else {
                     Toast.makeText(this, "Registration Failed", Toast.LENGTH_SHORT).show()
                 }
