3812db8f86dadd9c0483069e664fcd92794c416f
farhaankaleem <farhaan.kaleem@gmail.com>
2024-04-29 04:35:16 +0100

    Introduced Firebase Realtime DB

--- app/src/main/java/com/example/healthfitapplication/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/healthfitapplication/ui/home/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.example.healthfitapplication.ui.home
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.healthfitapplication.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import java.time.LocalDate
 
 class HomeFragment : Fragment() {
 
@@ -17,6 +23,10 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,6 +38,33 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+
+        val currentDate = LocalDate.now().toString()
+        val currentUser = auth.currentUser
+        currentUser?.let { user ->
+            val userId = user.uid
+            val loggedDateRef =
+                database.child("users").child(userId).child("loggedDate")
+            loggedDateRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val loggedDate = dataSnapshot.getValue(String::class.java)
+                    if (loggedDate != currentDate) {
+                        updateLoggedDate(userId, currentDate)
+                        resetToZero(userId)
+                    }
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    Log.e(
+                        "WorkoutExplain",
+                        "Failed to read logged-in date: ${databaseError.message}"
+                    )
+                }
+            })
+        }
+
         val textView: TextView = binding.textHome
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
@@ -38,5 +75,52 @@ class HomeFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun updateLoggedDate(userId: String, currentDate: String) {
+        val LogDateRef =
+            database.child("users").child(userId).child("loggedDate")
+        LogDateRef.setValue(currentDate)
+            .addOnSuccessListener {
+                Log.d("WorkoutExplain", "Logged Date set successfully")
+            }
+            .addOnFailureListener { e ->
+                Log.e("WorkoutExplain", "Failed to set logged date: ${e.message}")
+            }
+    }
+
+    private fun resetToZero(userId: String) {
+        val calBurntRef =
+            database.child("users").child(userId).child("totalCaloriesBurned")
+
+        val waterCountRef =
+            database.child("users").child(userId).child("currentWaterCount")
+
+        val stepsRef =
+            database.child("users").child(userId).child("stepsTaken")
+
+        calBurntRef.setValue(0)
+            .addOnSuccessListener {
+                Log.d("HomeFragment", "Total Calories Burnt reset successfully")
+            }
+            .addOnFailureListener { e ->
+                Log.e("HomeFragment", "Failed to reset Total Calories Burnt: ${e.message}")
+            }
+
+        waterCountRef.setValue(0)
+            .addOnSuccessListener {
+                Log.d("HomeFragment", "Water count reset successfully")
+            }
+            .addOnFailureListener { e ->
+                Log.e("HomeFragment", "Failed to reset Water count: ${e.message}")
+            }
+
+        stepsRef.setValue(0)
+            .addOnSuccessListener {
+                Log.d("HomeFragment", "Total Steps reset successfully")
+            }
+            .addOnFailureListener { e ->
+                Log.e("HomeFragment", "Failed to reset Total Steps: ${e.message}")
+            }
     }
 }
--- app/src/main/java/com/example/healthfitapplication/ui/running/RunningFragment.kt
+++ app/src/main/java/com/example/healthfitapplication/ui/running/RunningFragment.kt
@@ -5,13 +5,19 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.healthfitapplication.databinding.ActivityRunningBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import java.time.LocalDate
 
 class RunningFragment : Fragment(), SensorEventListener {
 
@@ -19,6 +25,10 @@ class RunningFragment : Fragment(), SensorEventListener {
     private val binding get() = _binding!!
     private var sensor: Sensor? = null
     private var sensorMgr: SensorManager? = null
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+    private var totalSteps: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,8 +39,29 @@ class RunningFragment : Fragment(), SensorEventListener {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference
+
+        val currentUser = auth.currentUser
+        currentUser?.let { user ->
+            val userId = user.uid
+            val stepsCountRef = database.child("users").child(userId).child("stepsGoal")
+
+            stepsCountRef.addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    totalSteps = snapshot.getValue(Int::class.java) ?: 0
+                    binding.totalSteps.text = "/"+totalSteps.toString()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
+        }
 
         sensorMgr = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensor = sensorMgr!!.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
@@ -49,6 +80,19 @@ class RunningFragment : Fragment(), SensorEventListener {
         event?.let {
             val stepsTaken = it.values[0]
             binding.stepsTaken.text = stepsTaken.toString()
+            val currentUser = auth.currentUser
+            currentUser?.let { user ->
+                val userId = user.uid
+                val stepsTakenRef = database.child("users").child(userId).child("stepsTaken")
+                stepsTakenRef.setValue(stepsTaken)
+                    .addOnSuccessListener {
+                        Log.d("RunningFragment", "Steps count updated successfully")
+                    }
+                    .addOnFailureListener { e ->
+                        Log.e("RunningFragment", "Failed to update steps count: ${e.message}")
+                        Toast.makeText(requireContext(), "Failed to update steps count: ${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+            }
         }
     }
 
